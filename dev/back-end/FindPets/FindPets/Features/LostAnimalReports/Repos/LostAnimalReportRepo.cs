44deab2387bbadb2b1563061254ed82b9630b4e6
Felipe <felipefalquetti@hotmail.com>
2024-05-07 23:09:59 -0300

    Criado cadastro de LostAnimalReports.

---
+++ dev/back-end/FindPets/FindPets/Features/LostAnimalReports/Repos/LostAnimalReportRepo.cs
@@ -0,0 +1,14 @@
+﻿using CoreApi.Infra.Context;
+using CoreAPI.Features.LostAnimalReports.Contracts;
+using CoreAPI.Features.LostAnimalReports.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreAPI.Features.LostAnimalReports.Repos;
+
+public class LostAnimalReportRepo(DataContext context) : ILostAnimalReportRepo
+{
+    private readonly DbSet<LostAnimalReport> DataSet = context.LostAnimalReports;
+
+    public async Task<IList<LostAnimalReport>> ListLostAnimalReportAsync() 
+        => await DataSet.ToListAsync();
+}
---
+++ dev/back-end/FindPets/FindPets/Shared/Enums/AnimalSize.cs
@@ -0,0 +1,6 @@
+﻿namespace CoreAPI.Shared.Enums;
+
+public enum AnimalSize
+{
+    Small = 1, Medium = 2, Large = 3
+}
